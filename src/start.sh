54f6751f49794ed7d2bb023810afe82abca886b9
Josquin IMBERT <josquin.imbert@gmail.com>
2023-03-03 15:32:11 +0100

    Adding Common Automatic Updates

--- src/start.sh
+++ src/start.sh
@@ -3,5 +3,5 @@
 # This script should be called at boot by the VM
 
 git -C /root/HoneyWalt_VM/ reset --hard
-git -C /root/HoneyWalt_VM/ pull
+git -C /root/HoneyWalt_VM/ pull --recurse-submodules
 python3 /root/HoneyWalt_VM/src/honeywalt_vm.py
