d036aef8aff76605a6a3e18cc9da61ea759d4176
Josquin IMBERT <josquin.imbert@gmail.com>
2023-06-30 14:11:51 +0200

    Handling no device for firewall

--- src/tools/firewall.py
+++ src/tools/firewall.py
@@ -1,4 +1,5 @@
 from common.utils.files import *
+from common.utils.logs import *
 from common.utils.system import *
 
 class Firewall:
@@ -14,8 +15,11 @@ class Firewall:
 		self.ips = []
 		for honeypot in self.server.config:
 			self.ips += [honeypot["device"]["ip"]]
-		self.str_ips = ",".join(self.ips)
-		run(to_root_path("src/script/firewall-up.sh")+" "+self.str_ips)
+		if len(self.ips) > 0:
+			self.str_ips = ",".join(self.ips)
+			run(to_root_path("src/script/firewall-up.sh")+" "+self.str_ips)
+		else:
+			log(WARNING, "Firewall.up: tried to start the firewall while no IP is known")
 
 	def stop(self):
 		if len(self.str_ips) > 0:
